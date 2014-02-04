3cb0239070b0e19ad32842ed35e9e084ff85f3a3
Jon Wong <j@jnwng.com>
2014-02-04 11:37:19 -0800

    If we don't have a define, expose it on the window.

--- Gruntfile.js
+++ Gruntfile.js
@@ -203,6 +203,8 @@ module.exports = function(grunt) {
       '  define([], function() {',
       '    return vjs;',
       '  });',
+      '} else {',
+      ' window.vjs = window.videojs = vjs;',
       '}',
       '})(window);'
     ].join('\n');
