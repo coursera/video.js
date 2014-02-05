4d979e62023027c9709ca93d1325a7507cf7641d
Jon Wong <j@jnwng.com>
2014-02-04 22:47:29 -0800

    Fixing indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -394,7 +394,7 @@ module.exports = function(grunt) {
       '    return vjs;',
       '  });',
       '} else {',
-      ' window.vjs = window.videojs = vjs;',
+      '  window.vjs = window.videojs = vjs;',
       '}',
       '})(window);'
     ].join('\n');
