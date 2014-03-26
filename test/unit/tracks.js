7cfe25dc46eb81a93607a14472cafbb8445465d1
Jon Wong <j@jnwng.com>
2014-03-26 00:40:14 -0700

    Making sure that the title element gets placed into the UL element
    Included are two sanity check tests for the two areas being checked
    to make sure that the menu title items are being correctly placed
    in the actual UL element of the menu.

---
+++ test/unit/tracks.js
@@ -0,0 +1,17 @@
+module('Tracks');
+
+test('should place title list item into ul', function() {
+  var player, chaptersButton;
+
+  player = PlayerTest.makePlayer();
+
+  chaptersButton = new vjs.ChaptersButton(player);
+
+  var menuContentElement = chaptersButton.el().getElementsByTagName('UL')[0];
+  var titleElement = menuContentElement.getElementsByClassName(
+    'vjs-menu-title')[0];
+
+  ok(titleElement.innerHTML === 'Chapters', 'title element placed in ul');
+
+  player.dispose();
+});
